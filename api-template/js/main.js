d1ab01acdcf8d841bd2a6218f6df8b58cf9408dd
regromrob <romae31@gmail.com>
2021-03-21 20:45:03 -0400

    async/await

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -1,24 +1,39 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+// //Example fetch using pokemonapi.co
+// document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+// function getFetch(){
+//   const choice = document.querySelector('#choice').value
+//   // console.log(choice)
+//   const url = 'http://www.boredapi.com/api/activity?type='+choice
+
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+//         console.log(data)
+//         document.querySelector('h2').innerText=data.activity
+//         // if(choice==='music'){
+//         //   document.querySelector('h2').innerText=data.type
+        
+//         // }
+        
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
+
+document.querySelector('button').addEventListener('click', boredom)
+
+async function boredom(){
   const choice = document.querySelector('#choice').value
-  // console.log(choice)
-  const url = 'http://www.boredapi.com/api/activity?type='+choice
+  const res = await fetch('http://www.boredapi.com/api/activity?type='+ choice)
+  const data = await res.json()
+  console.log(data)
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.querySelector('h2').innerText=data.activity
-        // if(choice==='music'){
-        //   document.querySelector('h2').innerText=data.type
-        
-        // }
-        
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+  try{
+    console.log('i work')
+  } catch(error){
+    console.error(error)
+  }
 }
-
+boredom()
